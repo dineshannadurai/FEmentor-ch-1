10aebe76beb5ba145b570544cb288692420234e7
Dinesh A <dineshmutthurasu@gmail.com>
2021-10-18 23:36:27 +0530

    CI(build): config done for build and deploy in gh-pages

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./index.js",
+    devServer: {
+        contentBase: "./",
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+    ],
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    }
+};
